fdf45a118d4edaa283a1da3023e77ddbec380ac1
Jay Greentree <JayGreentree@users.noreply.github.com>
2021-02-20 00:06:32 -0500

    New site

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,13 @@
+class ApplicationController < ActionController::Base
+	def page_title
+		"Thursday Church"
+	end
+
+	def site_title
+		"Thursday Church"
+	end
+
+	def page_description
+    	"Doing Church Differently while staying true to the message of Jesus Christ"
+	end
+end
